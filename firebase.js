cd14e2b535330455e0e2c19a8ecee54274d67627
breynersmith <breynersmithusariz@gmail.com>
2025-01-30 22:53:43 -0500

    variables de entorno agregadas

--- firebase.js
+++ firebase.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCu_g7byuEQQXu4ohPPLv7txZBKHHhB5Dc",
-  authDomain: "pruebachat-react.firebaseapp.com",
-  databaseURL: "https://pruebachat-react-default-rtdb.firebaseio.com",
-  projectId: "pruebachat-react",
-  storageBucket: "pruebachat-react.appspot.com",
-  messagingSenderId: "561841592377",
-  appId: "1:561841592377:web:69cf1a8b89e6358e9fd5e8"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
