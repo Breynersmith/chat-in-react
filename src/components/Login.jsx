1e1be42ea2be7bbb6f35242ab96577b1646fe688
breynersmith <breynersmithusariz@gmail.com>
2025-01-31 05:40:06 -0500

    login.jsx mmodificado

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,37 +1,58 @@
 import React, { useState, useContext } from "react";
-import { getDatabase, ref, push } from "firebase/database";
+import { getDatabase, ref, push, get } from "firebase/database";
 import { database } from '../../firebase';
 import { UserContext } from "../store/userContext.jsx"; 
 import './Login.css';
-import Header from './Header'
+import Header from './Header';
 
 const Login = () => {
   const [tempUsername, setTempUsername] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const { setUsername } = useContext(UserContext);
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
+    setErrorMessage('');
+
     if (tempUsername.trim() === '') {
-      alert("Por favor ingresa un nombre de usuario");
+      setErrorMessage("Por favor ingresa un nombre de usuario");
       return;
     }
 
+    const usersRef = ref(database, 'users');
 
-    push(ref(database, 'users'), { username: tempUsername });
+    try {
+      const snapshot = await get(usersRef);
+      if (snapshot.exists()) {
+        const users = Object.values(snapshot.val());
 
-    
-    setUsername(tempUsername);
+        const usernameExists = users.some(user => user.username === tempUsername);
+        
+        if (usernameExists) {
+          setErrorMessage("El nombre de usuario ya está en uso. Elige otro.");
+          return;
+        }
+      }
+
+      await push(usersRef, { username: tempUsername });
+
+      setUsername(tempUsername);
+    } catch (error) {
+      console.error("Error al verificar el usuario:", error);
+      setErrorMessage("Hubo un error al verificar el usuario. Inténtalo de nuevo.");
+    }
   };
 
   return (
     <div className="login-container">
       <div className="login">
-          <Header />
+        <Header />
         <input
           type="text"
           value={tempUsername}
           onChange={(e) => setTempUsername(e.target.value)}
           placeholder="Ingrese un nombre de usuario"
         />
+        {errorMessage && <p className="error">{errorMessage}</p>}
         <button onClick={handleLogin}>Entrar</button>
       </div>
     </div>
